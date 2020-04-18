81c0201a2b4f6133edf444d886111a67184b34a3
jrmkim50 <jrmkim50@gmail.com>
2020-04-18 19:56:01 -0400

    Initial Commit

---
+++ BoilerplateSocialApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BoilerplateSocialApp
+//
+//  Created by Min on 4/18/20.
+//  Copyright © 2020 Min. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
